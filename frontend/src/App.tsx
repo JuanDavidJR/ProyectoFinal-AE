f5c9bd42d4437eca30d57beaaa465abe0793ffa3
JuanDavidJR <juandajaimes10@gmail.com>
2025-06-04 18:22:15 -0500

    chore: estructura del repositorio

--- frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import Card from "./components/Card";
-function App() {
-  return <Card />;
-}
-
-export default App;
--- frontend/src/Titulo.tsx
+++
@@ -1,6 +0,0 @@
-function Titulo(){
-    const nombre = 'MEDIAGENDA';  
-    return <h1>{nombre}</h1>
-  }
-  
-  export default Titulo;
